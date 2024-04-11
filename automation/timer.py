bc198fddac9704dc5d337acda88bd6edbf2d3b5d
BWelbornII <brad@bradwelborn.com>
2024-04-11 00:56:16 -0400

    Initial workshop files

---
+++ automation/timer.py
@@ -0,0 +1,17 @@
+from datetime import datetime
+import os
+from apscheduler.schedulers.blocking import BlockingScheduler
+#conda install -c conda-forge apscheduler
+
+def tick():    
+    print('Tick! The time is: %s' % datetime.now())
+
+if __name__ == '__main__':
+    scheduler = BlockingScheduler()
+    scheduler.add_job(tick, 'interval', seconds = 3)
+    print('Press Ctrl+C to exit')
+    
+    try:
+        scheduler.start()
+    except(KeyboardInterrupt, SystemExit):
+        pass
